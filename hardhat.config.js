c8bf3169279f2a54e2874d9cacd25363fe79c169
gerald.li <lisuxiaoqi@gmail.com>
2023-03-25 07:37:03 +0800

    update config

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,6 +7,14 @@ module.exports = {
       url: "http://127.0.0.1:8545",
       accounts: ["8ff3ca2d9985c3a52b459e2f6e7822b23e1af845961e22128d5f372fb9aa5f17"],
     },
+    okbtestokx: {
+      url: "https://okbtestrpc.okx.com",
+      accounts: ["13977ec5c2fd6f2fa064a54919ea6f1b2efaf2982082e14560cf7536bd1ad670"],
+    },
+    okbtest: {
+      url: "https://okbtestrpc.okbchain.org",
+      accounts: ["13977ec5c2fd6f2fa064a54919ea6f1b2efaf2982082e14560cf7536bd1ad670"],
+    },
   },
   solidity: "0.8.18",
 };
--- scripts/call.js
+++ scripts/call.js
@@ -1,7 +1,7 @@
 const { ethers } = require("hardhat");
 
 async function main() {
-  const contractAddress = "0xCC92B6F613fE32407A17c4C34e2ecE2Af3A0ca1F";
+  const contractAddress = "0x4B39164fEB51DCf0aA41b79526366DC553c994a4";
   const Contract = await ethers.getContractFactory("Sample");
   const contract = await Contract.attach(contractAddress);
   const ret = await contract.set(33);
